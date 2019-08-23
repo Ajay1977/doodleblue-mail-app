2621760d43283cf1aab3f607def0ecf45da37620
Ajay Venkatesan <ajay.0211vijay@gmail.com>
2019-08-23 22:59:30 +0530

    Update

--- src/mail-details/side-nav/SideNav.js
+++ src/mail-details/side-nav/SideNav.js
@@ -3,16 +3,19 @@ import './sidenav.css';
 import { withRouter } from 'react-router-dom';
 
 function SideNav(props) {
-    const { inbox, sent, history, user } = props;
+    const { inbox, sent, history, user, location } = props;
+
+    console.log(location.pathname);
+
     return (
         <div className='side-nav-bar'>
             <button id='composeMailBtn' onClick={() => props.composeMail()}>Compose</button>
             <ul>
-                <li className="side-nav-list active" onClick={() => history.push(`/mailApp/inbox/${user.userId}`)}>
+                <li className={location.pathname.indexOf('/inbox/') > 0 ? 'active' : ''} onClick={() => history.push(`/mailApp/inbox/${user.userId}`)}>
                     Inbox
                     <span className='unread-mail' id='unreadMailInbox'>{inbox}</span>
                 </li>
-                <li className="side-nav-list" onClick={() => history.push(`/mailApp/sent/${user.userId}`)}>
+                <li className={location.pathname.indexOf('/sent/') > 0 ? 'active' : ''} onClick={() => history.push(`/mailApp/sent/${user.userId}`)}>
                     Sent
                     <span className='unread-mail' id='unreadMailSent'>{sent}</span>
                 </li>
