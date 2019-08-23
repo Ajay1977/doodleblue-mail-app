0cb2d8ec6eacd891d75229e35b289d530d96c137
Ajay Venkatesan <ajay.0211vijay@gmail.com>
2019-08-23 23:23:46 +0530

    Updates

---
+++ precache-manifest.73d6639b4d7585b454f4675ee789cac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be79c15a74b10b61e6077465e599766",
+    "url": "/doodleblue-mail-app/index.html"
+  },
+  {
+    "revision": "666319ec1991a07639c3",
+    "url": "/doodleblue-mail-app/static/css/main.9ee87a42.chunk.css"
+  },
+  {
+    "revision": "a6033ce019307d03a31f",
+    "url": "/doodleblue-mail-app/static/js/2.f5e83c7e.chunk.js"
+  },
+  {
+    "revision": "666319ec1991a07639c3",
+    "url": "/doodleblue-mail-app/static/js/main.bd795f9e.chunk.js"
+  },
+  {
+    "revision": "be818dacd85539ddaf95",
+    "url": "/doodleblue-mail-app/static/js/runtime~main.50d3198c.js"
+  },
+  {
+    "revision": "8f900bba1e994290a3d56c01d8c85908",
+    "url": "/doodleblue-mail-app/static/media/doodlebluelogo.8f900bba.png"
+  }
+]);
