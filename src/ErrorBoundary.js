d2548aaac606f4a51715c47d945e936ad844d810
Ajay Venkatesan <ajay.0211vijay@gmail.com>
2019-08-23 19:55:01 +0530

    Dashboard Changes

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+      console.log(props);
+    }
+  
+    static getDerivedStateFromError(error) {
+      return { hasError: true };
+    }
+
+    render() {
+      if (this.state.hasError) {
+        return (
+            <div className='error-container'>
+                <h1 style={{margin: '10px 0px'}}>Oops!!! Something went wrong.</h1>
+                <h3 style={{margin: '10px 0px'}}>Click below to go to Home page</h3>
+                <button style={{margin: '10px 0px', maxWidth: '200px'}} id="createAccBtn" onClick={() => {this.props.history.push('/'); document.location.reload();}}>Go to HomePage</button>
+            </div>
+        );
+      }
+  
+      return this.props.children; 
+    }
+}
+
+export default withRouter(ErrorBoundary);
